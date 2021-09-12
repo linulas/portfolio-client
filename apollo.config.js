6b3ecd13588b56e0eb43fe02a2c26ace1e8f2245
linulas <linubran90@gmail.com>
2021-09-12 22:18:17 +0200

    use gatsby api instead of strapi

--- apollo.config.js
+++ apollo.config.js
@@ -2,8 +2,8 @@ module.exports = {
     client: {
       name: 'Portfolio client',
       service: {
-        name: 'strapi-api',
-        url: 'http://localhost:1337/graphql',
+        name: 'graphql-api',
+        url: 'http://localhost:8000/__graphql',
       },
     },
   };
