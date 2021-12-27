630ad9c3a0a8033f0620fc6f384b92815e26cc19
linulas <linubran90@gmail.com>
2021-12-27 12:41:55 +0100

    fix types

--- src/graphql/MediaEntityRes.ts
+++ src/graphql/MediaEntityRes.ts
@@ -11,6 +11,7 @@ export interface MediaEntityRes_data_attributes {
   __typename: "Strapi_UploadFile";
   url: string;
   alternativeText: string | null;
+  formats: any | null;
 }
 
 export interface MediaEntityRes_data {
--- src/graphql/MediaRelRes.ts
+++ src/graphql/MediaRelRes.ts
@@ -11,6 +11,7 @@ export interface MediaRelRes_data_attributes {
   __typename: "Strapi_UploadFile";
   url: string;
   alternativeText: string | null;
+  formats: any | null;
 }
 
 export interface MediaRelRes_data {
