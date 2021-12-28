e7a208bd173e581ccf76a76a44469f4695a33566
linulas <linubran90@gmail.com>
2021-12-28 02:36:20 +0100

    ads nav styles

--- src/components/other/header.tsx
+++ src/components/other/header.tsx
@@ -1,9 +1,9 @@
-import * as React from "react";
+import * as React from 'react';
 
-import { breakpoints } from "../../styles/variables";
-import { useEffect, useState, useRef } from "react";
-import { MenuIcon, MenuOpenIcon } from "./MenuIcons";
-import { hasWindow } from "../../helpers";
+import { breakpoints } from '../../styles/variables';
+import { useEffect, useState, useRef } from 'react';
+import { MenuIcon, MenuOpenIcon } from './MenuIcons';
+import { hasWindow } from '../../helpers';
 
 interface HeaderProps {
   title: string;
@@ -21,15 +21,12 @@ export const Header: React.FC<HeaderProps> = ({ title }) => {
 
   // todo - use ref?
   const handleScroll = () => {
-    const about = document.getElementById("about");
-    const projects = document.getElementById("projects");
-    const contact = document.getElementById("contact");
+    const about = document.getElementById('about');
+    const projects = document.getElementById('projects');
+    const contact = document.getElementById('contact');
     const topOffset = document.body.getBoundingClientRect().top;
-    const bottom =
-      hasWindow &&
-      window.innerHeight + window.scrollY >= document.body.offsetHeight;
-    const topTrigger =
-      hasWindow && window.innerWidth > breakpoints.sm ? -150 : -25;
+    const bottom = hasWindow && window.innerHeight + window.scrollY >= document.body.offsetHeight;
+    const topTrigger = hasWindow && window.innerWidth > breakpoints.sm ? -150 : -25;
     const aboutOffset = about && about.getBoundingClientRect().top;
     const projectsOffset = projects && projects.getBoundingClientRect().top;
     const contactOffset = contact && contact.getBoundingClientRect().top;
@@ -66,38 +63,64 @@ export const Header: React.FC<HeaderProps> = ({ title }) => {
   };
 
   useEffect(() => {
-    hasWindow && window.addEventListener("scroll", handleScroll);
-    document.addEventListener("mousedown", handleClickOutside);
+    hasWindow && window.addEventListener('scroll', handleScroll);
+    document.addEventListener('mousedown', handleClickOutside);
     return () => {
-      hasWindow && window.removeEventListener("scroll", handleScroll);
-      document.removeEventListener("mousedown", handleClickOutside);
+      hasWindow && window.removeEventListener('scroll', handleScroll);
+      document.removeEventListener('mousedown', handleClickOutside);
     };
   }, [top, active, wrapperRef]);
 
   return (
-    <div ref={wrapperRef}>
-      <div className="flex">
+    <div
+      ref={wrapperRef}
+      className={`${
+        top ? 'h-20 bg-transparent' : 'h-16 bg-blue-primary'
+      } fixed w-screen top-0 z-10 flex items-center transition-all duration-300 ease-in-out`}
+    >
+      <div className={'w-1/4'}>
         <a
-          className="justify-end"
+          className={`p-4 uppercase font-bold text-bronze-primary hover:text-bronze-hover transition-all ${
+            top ? 'text-4xl' : 'text-2xl'
+          } duration-300 ease-in-out`}
           onClick={() => toggleOpen(false)}
           href="#top"
         >
           {title}
         </a>
-        <div className="flex justify-end" onClick={() => toggleOpen(false)}>
-          <div>
-            <a href="#about">About</a>
-          </div>
-          <div>
-            <a href="#projects">Projects</a>
-          </div>
-          <div>
-            <a href="#contact">Contact</a>
-          </div>
+      </div>
+      <div className="w-3/4 flex justify-end" onClick={() => toggleOpen(false)}>
+        <div>
+          <a
+            className={`p-3 uppercase font-bold text-white rounded-3xl ${active.about && 'bg-bronze-primary'}`}
+            href="#about"
+          >
+            About
+          </a>
         </div>
-        <div onClick={() => toggleOpen(!open)}>
-          {open ? <MenuOpenIcon /> : <MenuIcon />}
+        <div>
+          <a
+            className={`p-3 uppercase font-bold text-white rounded-3xl ${
+              active.projects && 'bg-bronze-primary'
+            }`}
+            href="#projects"
+          >
+            Projects
+          </a>
         </div>
+        <div>
+          <a
+            className={`p-3 uppercase font-bold text-white rounded-3xl ${
+              active.contact && 'bg-bronze-primary'
+            }`}
+            href="#contact"
+          >
+            Contact
+          </a>
+        </div>
+      </div>
+      <div onClick={() => toggleOpen(!open)} className="sm:hidden">
+        {open ? <MenuOpenIcon /> : <MenuIcon />}
       </div>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,31 @@
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        black: {
+          primary: "#030406",
+          hover: "#191A1C",
+        },
+        blue: {
+          primary: "#254984",
+          hover: "#607AA5",
+        },
+        bronze: {
+          primary: "#CD7F32",
+          hover: "#DAA169",
+        },
+        cyan: "#32CDCD",
+        green: "#2EBB74",
+        pink: "#BB2E75",
+        platinum: "#E8E8E8",
+        silver: {
+          primary: "#C0C0C0",
+          hover: "#E2E2E2",
+        },
+        white: "#EFF1F3",
+      },
+    },
   },
   plugins: [],
-}
+};
