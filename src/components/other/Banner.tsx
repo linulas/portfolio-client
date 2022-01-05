22cf9c4109c727f8e997f2a069f779ca8e4cf0c8
linulas <linubran90@gmail.com>
2022-01-05 19:56:14 +0100

    smaller banner size

--- src/components/other/Banner.tsx
+++ src/components/other/Banner.tsx
@@ -45,7 +45,7 @@ const query = graphql`
   query Banner {
     image: file(relativePath: { eq: "img/coding-bg.jpg" }) {
       childImageSharp {
-        fluid(maxWidth: 2000, quality: 100) {
+        fluid(maxWidth: 1000, quality: 100) {
           ...GatsbyImageSharpFluid
         }
       }
