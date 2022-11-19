5bb5db485c4dfea43e9670cc84c0be643aac4847
linulas <linubran90@gmail.com>
2022-11-19 18:16:28 +0100

    ads Grid component

---
+++ src/lib/Grid.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	export let colSize: 'sm' | 'md' | 'lg' = 'md';
+</script>
+
+<div class="grid colSize-{colSize}">
+	<slot />
+</div>
+
+<style lang="scss">
+	.grid {
+		display: grid;
+		grid-template-columns: repeat(auto-fit, minmax(25rem, 1fr));
+		gap: 2rem;
+		&.colSize-sm {
+			grid-template-columns: repeat(auto-fit, minmax(14rem, 1fr));
+		}
+		&.colSize-md {
+			grid-template-columns: repeat(auto-fit, minmax(18rem, 1fr));
+		}
+		&.colSize-lg {
+			grid-template-columns: repeat(auto-fit, minmax(25rem, 1fr));
+		}
+	}
+</style>
