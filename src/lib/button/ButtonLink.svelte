cbf1f3c18ab17235d9730ba51a87ee2ecfb53a6a
linulas <linubran90@gmail.com>
2022-11-19 14:56:59 +0100

    ads ButtonLink

---
+++ src/lib/button/ButtonLink.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	export let href = '';
+</script>
+
+<a {href} class="button">
+	<slot />
+</a>
+
+<style lang="scss">
+	@import './buttonBase.scss';
+
+	a {
+		display: inline-block;
+		text-decoration: none;
+	}
+</style>
