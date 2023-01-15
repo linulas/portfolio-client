295c2dc729dc332c0e995bc6b1c251b8f9885ed8
linulas <linubran90@gmail.com>
2023-01-15 13:55:15 +0100

    ads oneTimeObserver

--- src/lib/helpers/viewport.ts
+++ src/lib/helpers/viewport.ts
@@ -1,24 +1,50 @@
-let intersectionObserver: IntersectionObserver;
+let constantObserver: IntersectionObserver;
+let oneTimeObserver: IntersectionObserver;
 
-function ensureIntersectionObserver() {
-	if (intersectionObserver) return;
-
-	intersectionObserver = new IntersectionObserver((entries) => {
-		entries.forEach((entry) => {
-			const eventName = entry.isIntersecting ? 'enter' : 'exit';
-			entry.target.dispatchEvent(new CustomEvent(eventName));
-		});
-	});
+interface options {
+	unobserveAfterEnter: boolean;
 }
 
-export default function viewport(element: Element) {
-	ensureIntersectionObserver();
+function ensureIntersectionObserver({ unobserveAfterEnter }: options) {
+	if (unobserveAfterEnter) {
+		if (oneTimeObserver) return;
 
-	intersectionObserver.observe(element);
+		oneTimeObserver = new IntersectionObserver((entries) => {
+			entries.forEach((entry) => {
+				const eventName = entry.isIntersecting ? 'enter' : 'exit';
+				entry.target.dispatchEvent(new CustomEvent(eventName));
+
+				if (eventName === 'enter') oneTimeObserver.unobserve(entry.target);
+			});
+		});
+	} else {
+		if (constantObserver) return;
+
+		constantObserver = new IntersectionObserver((entries) => {
+			entries.forEach((entry) => {
+				const eventName = entry.isIntersecting ? 'enter' : 'exit';
+				entry.target.dispatchEvent(new CustomEvent(eventName));
+			});
+		});
+	}
+}
+
+export default function viewport(element: Element, opts: options = { unobserveAfterEnter: false }) {
+	ensureIntersectionObserver(opts);
+
+	if (opts.unobserveAfterEnter) {
+		oneTimeObserver.observe(element);
+	} else {
+		constantObserver.observe(element);
+	}
 
 	return {
 		destroy() {
-			intersectionObserver.unobserve(element);
+			if (opts.unobserveAfterEnter) {
+				oneTimeObserver.unobserve(element);
+			} else {
+				constantObserver.unobserve(element);
+			}
 		}
 	};
 }
