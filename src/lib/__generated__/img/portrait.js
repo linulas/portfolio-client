e7f7c1dcec454915256053e6edaf62e6e6da894a
linulas <linubran90@gmail.com>
2023-01-18 12:16:50 +0100

    fix portrait img quality issues

--- src/lib/__generated__/img/portrait.js
+++ src/lib/__generated__/img/portrait.js
@@ -1,8 +1,8 @@
 // @ts-nocheck
-import meta from '$lib/images/portrait.jpg?width=192&metadata';
+import meta from '$lib/images/portrait.jpg?width=720&metadata';
 import smallMeta from '$lib/images/portrait.jpg?width=384&metadata';
-import srcsetwebp from '$lib/images/portrait.jpg?w=192;&webp&srcset';
-import srcsetauto from '$lib/images/portrait.jpg?w=192;&jpeg&srcset';
+import srcsetwebp from '$lib/images/portrait.jpg?w=720;672;384&webp&srcset';
+import srcsetauto from '$lib/images/portrait.jpg?w=720;672;384&jpeg&srcset';
 import small from '$lib/images/portrait.jpg?width=384';
 import medium from '$lib/images/portrait.jpg?width=672';
 import large from '$lib/images/portrait.jpg?width=1440';
@@ -22,6 +22,6 @@ const data = {
   ],
   dominantColour: '#f8f8f8',
   placeholder:
-    'data:image/jpeg;base64,/9j/2wBDAAwREhMUFBUUFBofGhUaHiIcGhohKC4jJB4hMzg/PTouKC5CRFpMPi5XRTc3VmBRVlpgZmRkQklveXFmeFtlaWf/2wBDAQgMDQwNDw8PDxFgEBIaVmBaYFpgY2NjY2BjYGBiY2NjY2NjY2NjY2NjY2JjY2NjY2NjY2NjY2NjY2NjY2NjY2P/wgARCAALAAoDASIAAhEBAxEB/8QAFgABAQEAAAAAAAAAAAAAAAAABQYH/8QAFAEBAAAAAAAAAAAAAAAAAAAAA//aAAwDAQACEAMQAAAAVtMFDNf/xAAeEAACAgICAwAAAAAAAAAAAAABAgMEACEFESJCgf/aAAgBAQABPwC5zFVOQiCSdqnjM/qmLNEyhldCpAIIYbGUUVq11mHbBdH4cEjjQY5//8QAFxEBAAMAAAAAAAAAAAAAAAAAAQACIf/aAAgBAgEBPwANtP/EABYRAQEBAAAAAAAAAAAAAAAAAAIAMf/aAAgBAwEBPwBYb//Z',
+    'data:image/jpeg;base64,/9j/2wBDAAwREhMUFBUUFBofGhUaHiIcGhohKC4jJB4hMzg/PTouKC5CRFpMPi5XRTc3VmBRVlpgZmRkQklveXFmeFtlaWf/2wBDAQgMDQwNDw8PDxFgEBIaVmBaYFpgY2NjY2BjYGBiY2NjY2NjY2NjY2NjY2JjY2NjY2NjY2NjY2NjY2NjY2NjY2P/wgARCAAMAAoDASIAAhEBAxEB/8QAFgABAQEAAAAAAAAAAAAAAAAABQYH/8QAFAEBAAAAAAAAAAAAAAAAAAAAA//aAAwDAQACEAMQAAAAesMAMNf/xAAgEAEAAgMAAAcAAAAAAAAAAAABAgMABBEFISJBUnKR/9oACAEBAAE/ANzxbXq36ay45H02/GH2wlFBER8xPfNKEbNfdsmdmHR/cjtXxCJZIA4Gf//EABcRAQADAAAAAAAAAAAAAAAAAAEAAiH/2gAIAQIBAT8ADbT/xAAXEQADAQAAAAAAAAAAAAAAAAAAAQIx/9oACAEDAQE/AHkn/9k=',
 };
 export { data as default };
